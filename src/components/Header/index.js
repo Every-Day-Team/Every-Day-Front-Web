0a5866f1289ed8f8ceb93769d9a502b3c971c0f7
bm4656 <newjin46@gmail.com>
2022-09-14 23:57:19 +0900

    참고사이트 clone

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="App-header">
+        <div className="Header-logo">
+          <strong>Everyday-Mission</strong>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Status/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+class Status extends React.Component {
+  render() {
+    return (
+      <div className="Container-status">
+        <h1>동미대 님의 미션</h1>
+      </div>
+    );
+  }
+}
+
+export default Status;
