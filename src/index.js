5efee17028c868b8bd4df942f5178949be6f153d
bm4656 <newjin46@gmail.com>
2022-09-19 17:18:19 +0900

    fix: ReactDOM.render 18 오류 해결

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+// ReactDOM.render(
+// <App />, document.getElementById('root')
+// );
